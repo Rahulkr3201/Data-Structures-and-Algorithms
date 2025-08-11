5ff3d8cc6645f36e25cb73d1d1f1e531ef85d6c4
Rahul kumar <172767765+Rahulkr3201@users.noreply.github.com>
2025-08-11 14:04:17 +0530

    commit

---
+++ DynamicProgramming/coinchange2.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    int change(int amount, vector<int> &coins)
+    {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+        for (int T = 0; T <= amount; T++)
+        {
+            if (T % coins[0] == 0)
+                dp[0][T] = 1;
+        }
+        for (int ind = 1; ind < n; ind++)
+        {
+            for (int T = 0; T <= amount; T++)
+            {
+                long notTake = dp[ind - 1][T];
+                long take = 0;
+                if (coins[ind] <= T)
+                {
+                    take = dp[ind][T - coins[ind]];
+                }
+                dp[ind][T] = take + notTake;
+            }
+        }
+        return dp[n - 1][amount];
+    }
+};
