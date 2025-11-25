514d47f56066f44b6f9b6c08aa278dde2ae4f0db
Rahul kumar <172767765+Rahulkr3201@users.noreply.github.com>
2025-11-25 10:29:03 +0530

    subarraywithsubset1

---
+++ practice questions/smallestIntegerdivisiblebyk.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    int smallestRepunitDivByK(int k)
+    {
+        if (k % 2 == 0 || k % 5 == 0)
+            return -1;
+
+        vector<bool> visited(k, false);
+
+        int rem = 0;
+
+        for (int len = 1; len <= k; len++)
+        {
+            rem = (rem * 10 + 1) % k;
+
+            if (rem == 0)
+                return len;
+
+            if (visited[rem])
+                return -1;
+
+            visited[rem] = true;
+        }
+
+        return -1;
+    }
+};
